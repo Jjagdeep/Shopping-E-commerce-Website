8af78dbbb25883850bc45a3b99072e4138a106cc
Jagdeep <jjagdeep1997@gmail.com>
2022-05-20 00:32:14 +0530

    final commit

---
+++ 03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,9 @@
+export default {
+
+    oidc: {
+        clientId: '0oa4u7bfwkTRxbPZO5d7',
+        issuer: 'https://dev-86321128.okta.com/oauth2/default',
+        redirectUri: 'https://localhost:4200/login/callback',
+        scopes: ['openid','profile','email']
+    }
+}
